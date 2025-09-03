06f4f31432316845a13873cee96441773ccf5cc0
lozp1 <francolpz001@gmail.com>
2025-09-03 00:17:25 -0600

    Actualizando archivos

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-console.log("Ejecutando el sistema")
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
